1cec3c729476b0eceadd95d5807839bad9b41cc2
bencso <gabor.bencsik13@gmail.com>
2019-11-25 17:45:00 +0100

    insert into bigquery

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const bigquery = new BigQuery();
 
 exports.main = async (req, res) => {
 
-  // const url = `${req.protocol}://${req.hostname}/${req.originalUrl}`;
-  // const dateTime = new Date().toISOString();
-  // const queryString = `INSERT \`terraform-demo-project-258413.terraform_demo_example_dataset.visits\` (url, timestamp) VALUES('${url}', '${dateTime}')`;
-  // console.log(query)
+  const url = `${req.protocol}://${req.hostname}/${req.originalUrl}`;
+  const dateTime = new Date().toISOString();
+  const queryString = `INSERT \`terraform-demo-project-258413.terraform_demo_example_dataset.visits\` (url, timestamp) VALUES('${url}', '${dateTime}')`;
+  console.log(query)
 
-  // await bigquery.query(queryString, {} , (err, rows) => {
-  //   if (!err) {
-  //     console.error(err)
-  //   }
-  // });
+  await bigquery.query(queryString, {} , (err, rows) => {
+    if (!err) {
+      console.error(err)
+    }
+  });
 
   res.send('Hello Terraform!');
 };
