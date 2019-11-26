bcedbd780552bb18848bca030c152913259a90b2
bencso <gabor.bencsik13@gmail.com>
2019-11-26 15:39:14 +0100

    fix variable name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.main = async (req, res) => {
   const url = `${req.protocol}://${req.hostname}/${req.originalUrl}`;
   const dateTime = new Date().toISOString();
   const queryString = `INSERT \`terraform-demo-project-258413.terraform_demo_example_dataset.visits\` (url, timestamp) VALUES('${url}', '${dateTime}')`;
-  console.log(query)
+  console.log(queryString)
 
   await bigquery.query(queryString, {} , (err, rows) => {
     if (!err) {
